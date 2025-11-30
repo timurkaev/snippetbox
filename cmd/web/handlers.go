3751b74944ade1aa549880998593e5b2ec09aef3
Ismail_Timurkaev <timurkaev.Is@gmail.com>
2025-11-30 16:17:23 +0300

    added reover panic middleware

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -14,7 +14,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		app.notFound(w)
 		return
 	}
-
+	
 	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
