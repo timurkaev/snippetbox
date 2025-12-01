c8611e70a1f7f103d10a083f59d6c0e19f366c6f
Ismail_Timurkaev <timurkaev.Is@gmail.com>
2025-12-01 22:06:00 +0300

    added create snippet page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -49,23 +49,8 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
-
-	title := "O snail"
-	content := "O snail\nClimb Mound Fuji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
-	expires := 7
-
-	id, err := app.snippets.Insert(title, content, expires)
-	if err != nil {
-		app.serverError(w, err)
-		return
-	}
-
-	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+	data := app.newTemplateData(r)
+	app.render(w, http.StatusOK, "create.tmpl", data)
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
